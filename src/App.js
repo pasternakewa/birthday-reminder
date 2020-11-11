6768de5f394fa8a3f713fe026b7ee5d131d83490
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-11-11 19:02:24 +0100

    Centre web app content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      ></div>
+    );
   }
 }
 
