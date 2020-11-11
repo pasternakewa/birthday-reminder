fd1ca5e1d8f7e1d1daa47a0685ad53d5b5c202fc
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-11-11 19:10:03 +0100

    Rewrite App component to functional

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      ></div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    ></div>
+  );
+};
 
 export default App;
