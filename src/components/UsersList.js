78e54c6fbba0bc01a88673a8a6ff99c5b6ce3e3b
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-11-12 22:57:20 +0100

    Move initial data to state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const users = [
-    { name: "Leo", lastName: "DiCaprio", birthDate: new Date("1974-11-11") },
-    { name: "Demi", lastName: "Moore", birthDate: new Date("1962-11-11") },
-    { name: "Calista", lastName: "Flockhart", birthDate: new Date("1964-11-11") }
-  ];
+  { name: "Leo", lastName: "DiCaprio", birthDate: new Date("1974-11-11") },
+  { name: "Demi", lastName: "Moore", birthDate: new Date("1962-11-11") },
+  { name: "Calista", lastName: "Flockhart", birthDate: new Date("1964-11-11") }
+];
 const today = new Date();
 
 const UsersList = () => {
+  const [state, setState] = useState([...users]);
+
   return (
     <ul>
-      {users.map((user) => (
+      {state.map((user) => (
         <li key={user.name}>
           <div>{`${user.name} ${user.lastName}`}</div>
           <div>{`age: ${
