dff93f473bde80aae3aae974eff6854948074a06
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-11-26 23:05:26 +0100

    Move validation outside the component

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
+import validate from "../validate";
 
 const people = [
   { id: 1, name: "Leo", lastName: "DiCaprio", birthDate: "1974-11-11" },
@@ -29,48 +30,9 @@ const PeopleList = () => {
     setFormState({ ...formState, [key]: e.target.value });
   };
 
-  const validate = () => {
-    let isError = false;
-    const errors = [];
-    const dateRegex = /^(19[5-9][0-9]|20[0-1][0-9])[-/.](0?[1-9]|1[0-2])[-/.](0?[1-9]|[12][0-9]|3[01])$/gim;
-
-    //name validation
-    if (formState.name.length < 3) {
-      isError = true;
-      errors.nameError = "Imię za krótkie";
-    } else if (formState.name.length > 18) {
-      isError = true;
-      errors.nameError = "Imię za długie. Max 18 znaków.";
-    } else {
-      isError = false;
-      errors.nameError = "";
-    }
-
-    //lastName validation
-    if (formState.lastName.length < 3) {
-      isError = true;
-      errors.lastNameError = "Nazwisko za krótkie.";
-    } else if (formState.lastName.length > 18) {
-      isError = true;
-      errors.lastNameError = "Nazwisko za długie. Max 18 znaków.";
-    } else {
-      isError = false;
-      errors.lastNameError = "";
-    }
-
-    //birthDate validation
-    if (!dateRegex.test(formState.birthDate)) {
-      isError = true;
-      errors.birthDateError = "Niepoprawy format daty";
-    }
-
-    setFormState({ ...formState, ...errors });
-    return isError;
-  };
-
   const handleConfirm = () => {
     event.preventDefault();
-    const err = validate();
+    const err = validate(formState, setFormState);
     if (!err) {
       if (editId) {
         const updatedPeopleState = peopleState.reduce(
